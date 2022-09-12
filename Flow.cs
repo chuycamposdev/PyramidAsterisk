ce22be7b8c179f79bd46bdb4d4482bfcb1f290d4
chuycamposdev <111834596+chuycamposdev@users.noreply.github.com>
2022-09-12 17:51:46 -0500

    Add files via upload

---
+++ Flow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    public static class Flow
+    {
+        public static void PrintAsteriskTree(int rowsToPrint)
+        {
+            int currentRow = 1;
+            bool reduce = false;
+            while (currentRow >= 1)
+            {
+
+                for (int leftWhiteSpace = currentRow; leftWhiteSpace < rowsToPrint; leftWhiteSpace++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(new string('*', currentRow + (currentRow - 1)));
+
+                for (int rightWhiteSpace = currentRow; rightWhiteSpace < rowsToPrint; rightWhiteSpace++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (currentRow == rowsToPrint)
+                    reduce = true;
+
+                if (!reduce)
+                    currentRow++;
+
+                else
+                    currentRow--;
+                Console.WriteLine();
+            }
+        }
+    }
+}
